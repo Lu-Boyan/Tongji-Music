d42cf3b588cbdb598c5f8731d631d8512649b222
Lu-Boyan <376597962@qq.com>
2021-11-23 10:26:39 +0800

    仓库搭建

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!--路由占位符-->
+    <vue-particles
+        color="#fff"
+        :particleOpacity="0.7"
+        :particlesNumber="60"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#fff"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="2"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+    <router-view></router-view>
+  </div>
+</template>
+
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from '../components/login.vue'
+
+Vue.use(Router)
+
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    }
+  ]
+})
+
