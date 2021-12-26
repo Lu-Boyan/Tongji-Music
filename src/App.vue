d4a254533431dc68d8d53b83bdbba7ab51d41333
zf909 <80331399+zf909@users.noreply.github.com>
2021-12-26 20:05:51 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,43 @@
     <!--路由占位符-->
 
     <router-view></router-view>
+        <el-container >
+
+          <el-aside width="150px">
+            <el-button
+              style="margin-top:30px;margin-left:20px !important;"
+              class="el-icon-d-arrow-left"
+              size="large"
+              @click="lastSong()"
+              circle/>
+            <el-button
+              class="el-icon-d-arrow-right"
+              size="large"
+              @click="nextSong()"
+              circle/>
+          </el-aside>
+          <el-main>
+            <audio ref="audio" src="" style="width:100%; " controls="controls" @ended="nextSong()">
+              Your browser does not support the audio element.
+            </audio>
+                
+    
+          </el-main>
+        </el-container>
   </div>
 </template>
+
+<style scoped>
+.el-container
+{
+  margin-top: 0px;
+  background-color:skyblue;
+  /* position: fixed; */
+}
+  /* audio {
+    position: fixed;
+  } */
+
+</style>
+
 
